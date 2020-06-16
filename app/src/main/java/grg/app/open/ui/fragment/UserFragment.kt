f3c787e18e53ac16f860fd6ef4e343b94dbb0084
iso88591 <2658218247@qq.com>
2020-06-16 18:41:42 +0800

    文章详情页 添加 收藏按钮 但是还没做事件

---
+++ app/src/main/java/grg/app/open/ui/fragment/UserFragment.kt
@@ -0,0 +1,17 @@
+package grg.app.open.ui.fragment
+
+import android.os.Bundle
+import grg.app.open.R
+import grg.app.open.app.base.BaseFragment
+
+class UserFragment : BaseFragment(R.layout.fragment_user) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
